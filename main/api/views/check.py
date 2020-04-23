435a4bdadcef2d9a90a3f96fae17d7db28880314
Пользователь <pumalcn03@gmail.com>
2020-04-24 02:28:02 +0600

    35%

--- main/api/views/check.py
+++
@@ -1,5 +0,0 @@
-from django.http import HttpResponse
-
-
-def check(request):
-    return HttpResponse('Hello, World!')
--- main/users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from rest_framework_jwt.views import obtain_jwt_token
-
-from main.users.views import UserCreateView
-
-urlpatterns = [
-    path('login/', obtain_jwt_token),
-    path('register/', UserCreateView.as_view()),
-]
